6e15122dca55b2571935646974a6c9001d175ad3
Ty Jacobs <twjacobs@alaska.edu>
2021-01-15 19:09:46 -0900

    adds the favorite game description.

---
+++ HW0/game/game.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+
+int main()
+{	// favorite game is Stardew Valley
+	std::cout << "You've inherited your grandfater's old farm plot in" << std::endl;
+	std::cout << "Stardew Valley. Armed with hand-me-down tools and" << std::endl;
+	std::cout << "a few coins, you set out to begin your new life. Can" << std::endl;
+	std::cout << "you learn to live off the land and turn these" << std::endl;
+	std::cout << "overgrown fields into a thriving home?" << std::endl;
+}
+
