5a520192bd49885be889a8effc25955fb9a023b2
Ty Jacobs <twjacobs@alaska.edu>
2021-01-15 19:01:12 -0900

    added the poem part of the homwork 0

---
+++ HW0/poem/poem.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+
+int main()
+{
+	std::cout << "By this cunning change of hearts," << std::endl;
+	std::cout << "You the power of love control;" << std::endl;
+	std::cout << "While the boy's eluded darts" << std::endl;
+	std::cout << "Can arrive at neither soul," << std::endl;
+	std::cout << "By Edmund Waller, 'On the Friendship betwixt Two Ladies" << std::endl;
+	std::cout << "Choose this at random since I don't have a favorite poem" << std::endl;
+}
+/*
+poem by Edmund Waller, 'On the Friendship betwixt Two Ladies'
+
+I choose some random poem since I don't know any poem
+
+*/
