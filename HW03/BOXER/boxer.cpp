da3a6e769bc896d5fd14f7e61aa0e5a52104e98f
Ty Jacobs <twjacobs@alaska.edu>
2021-02-09 17:17:29 -0900

    add the boxer hpp and cpp files. and the boxprint file with the comments

---
+++ HW03/BOXER/boxer.cpp
@@ -0,0 +1,9 @@
+/*
+Ty Jacosb
+CS201
+boxer.cpp
+2/9/2021
+
+This program will get a string input and a positive number,
+Then it would print a box with the string in the middle
+*/
---
+++ HW03/BOXER/boxprint.cpp
@@ -0,0 +1,9 @@
+/*
+Ty Jacosb
+CS201
+boxprint.cpp
+2/9/2021
+
+This program will get a string input and a positive number,
+Then it would print a box with the string in the middle
+*/
