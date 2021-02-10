ee33684fb01b438d8fe6bd35c3b2462bbcc55e38
Ty Jacobs <twjacobs@alaska.edu>
2021-02-09 18:35:52 -0900

    Made it so user can enter in the word and number

--- HW03/BOXER/boxprint.cpp
+++ HW03/BOXER/boxprint.cpp
@@ -14,11 +14,15 @@ Then it would print a box with the string in the middle
 using std::cout;
 using std::endl;
 using std::string;
+using std::cin;
 
 int main() {
 
-	string word = "Hello!";
+	string word = "";
 	int number = num();
+	cout << "please enter a word: ";
+	cin >> word;
+	cout << endl;
 	printBox(word, number);
 	printMid(word, number);
 	printBox(word, number);
