aae93b48eb8a0488f28eb88a64e03c2737cdf3e1
Ty Jacobs <twjacobs@alaska.edu>
2021-02-27 11:43:19 -0900

    added additional Program 2 and random number from 0 to 9.

---
+++ HW04/additional Program 2/bulls-and-cows.cpp
@@ -0,0 +1,25 @@
+/*
+Ty Jacobs
+2/27/2021
+bulls-and-cows.cpp
+
+This program is a gussing game called bulls and cows which
+I have number heard of. 
+*/
+
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <vector>
+#include <stdlib.h>
+
+// gets random number from 0 to 9
+int numb() {
+	return rand() % 10;
+}
+
+int main() {
+
+	
+	return 0;
+}
