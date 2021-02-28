6a0987e80e2f7074b6835274673b1aaf13a35706
Ty Jacobs <twjacobs@alaska.edu>
2021-02-27 15:24:09 -0900

    Makes it that it will give out a diffrent Number for each of the four spots.

--- HW04/additional Program 2/bulls-and-cows.cpp
+++ HW04/additional Program 2/bulls-and-cows.cpp
@@ -11,15 +11,46 @@ I have number heard of.
 using std::cout;
 using std::endl;
 #include <vector>
+using std::vector;
 #include <stdlib.h>
+#include <time.h>
 
 // gets random number from 0 to 9
 int numb() {
 	return rand() % 10;
 }
 
-int main() {
+// sets the vector to have diffrent numbres
+void arr(vector<int> &num) {
+	num.at(0) = numb();
+	vector<int> dontuse;
+	dontuse.push_back(num.at(0));
+	// adds and checks each number
+	for (int i = 1; i < 4; i++) {
+		// makes random number to each part
+		num.at(i) = numb();
+		// check eachs number amd remakes them if the same.
+		for (int j = 0; j < dontuse.size(); j++) {
+			if (num.at(i) == dontuse.at(j)) {
+				// makes it that num is a diffrent number
+				while (num.at(i) == dontuse.at(j)) {
+					num.at(i) = numb();
+				}// end of while
+			}// end of if
+		}// end of for loop
+		dontuse.push_back(num.at(i));
+	}
+}// end of arr
 
-	
+int main() {
+	srand(time(0));
+	vector<int> rnum(4);
+	arr(rnum);
+
+	for (int i = 0; i < 4; i++) {
+		cout << rnum.at(i) << " ";
+	}
+	cout << endl;
+
 	return 0;
 }
