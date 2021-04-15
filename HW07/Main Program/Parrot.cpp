b704363d03e234ab040fc2d62694b17f21b62d60
Ty Jacobs <twjacobs@alaska.edu>
2021-04-14 16:26:02 -0800

    Adds #include stuff, adds int main

---
+++ HW07/Main Program/Parrot.cpp
@@ -0,0 +1,20 @@
+/*
+Ty Jacobs
+4/14/2021
+CS 201
+
+This program will open ppm and convert it to ascii. 
+*/
+
+#include <string>
+using std::string;
+#include <fstream>
+using std::ifstream;
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main() {
+
+	return 0;
+}
