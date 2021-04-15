ec507d2682b9dff8bebbd209616c99ee7ca07eb9
Ty Jacobs <twjacobs@alaska.edu>
2021-04-15 15:35:32 -0800

    Made it that it reads words from a line

--- Final Project/Count words/wordCount.cpp
+++ Final Project/Count words/wordCount.cpp
@@ -14,9 +14,41 @@ using std::string;
 #include <fstream>
 using std::ifstream;
 #include <map>
+using std::map;
 #include <iostream>
 using std::cout;
 using std::endl;
+#include <sstream>
+using std::istringstream;
+
+void toWords(string line, map<string, int> w) {
+	string word = "";
+
+	// does everything execpt for last word
+	for (char a : line) {
+		if (a != ' ') {
+			word += a;
+		}
+		else {
+			cout << word << endl;
+			word = "";
+		}
+	}
+
+	// double checks end of line to make check if theres a space or no
+	
+	//if (word.size() != 0) {
+	//
+	//}
+	bool lastW = true;
+	for (char a : word) {
+		if (a == ' ') {
+			lastW == false;
+		}
+	}
+	if (lastW) cout << word << endl;
+
+}
 
 int main() {
 	cout << "This will count the words from a file and output the results to a diffrent file." << endl;
@@ -25,6 +57,18 @@ int main() {
 	ifstream file(infile);
 	if (!file) {
 		cout << "File: " << infile << " not found" << endl;
+	}
+	cout << "Opend file: " << infile << endl;
+	cout << endl;
+
+	map<string, int> word;
+	string line;
+	while (getline(file,line)) {
+		cout << line << endl;
+		cout << endl;
+
+		toWords(line, word);
+	
 	}
 
 }
