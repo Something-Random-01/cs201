5e28d85b70925b373aee2ce5d6b22b6e46f3955d
Ty Jacobs <twjacobs@alaska.edu>
2021-04-19 11:46:24 -0800

    adds upper case shifting to CasarCypher

--- HW07/Caesar Cypher/CaesarCypher.cpp
+++ HW07/Caesar Cypher/CaesarCypher.cpp
@@ -30,16 +30,39 @@ void charLower(char& letter, int shift) {
 	}
 }
 
+void charUpper(char& letter, int shift) {
+	int overSize = 1;
+	if (letter >= 'A' && letter <= 'Z') {
+		if (letter + shift > 'Z') {
+			while (letter != 'Z') {
+				overSize++;
+				letter += 1;
+			}
+			letter = 'A' + shift - overSize;
+		}
+		else {
+			letter += shift;
+		}
+	}
+
+}
+
 int main() {
 	int shift = 3;
 	string line = "hello, everyone!";
+	string lineU = "Hello, everyone!";
 
 	// has errors if more than 27 and negative numbers
 	// does lower case
 	for (auto &l : line) {
 		charLower(l, shift);
 	}
+	for (auto& l : lineU) {
+		charUpper(l, shift);
+		charLower(l, shift);
+	}
 	cout << line << endl;
+	cout << lineU << endl;
 
 	return 0;
 }
