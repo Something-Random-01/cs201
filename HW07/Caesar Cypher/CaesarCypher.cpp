cccb2083a9c2bca348bd225ef7cd6b0dab83cf3f
Ty Jacobs <twjacobs@alaska.edu>
2021-04-19 11:04:53 -0800

    Does the letter shifting from z to a plus the shift

--- HW07/Caesar Cypher/CaesarCypher.cpp
+++ HW07/Caesar Cypher/CaesarCypher.cpp
@@ -13,7 +13,24 @@ using std::cout;
 using std::endl;
 
 int main() {
-	
+	int shift = 3;
+	string line = "z";
+	int overSize = 0;
+
+	for (auto &l : line) {
+		if (l >= 'a' && l <= 'z' || l >= 'A' && l <= 'Z') {
+			if (l + shift > 'z') {
+				overSize = 'z' - l + shift -1;
+				cout << overSize << " ";
+				l = 'a' + overSize;
+				cout << l << endl;
+			}
+			else {
+				l += shift;
+			}
+	}
+	}
+	cout << line << endl;
 
 	return 0;
 }
